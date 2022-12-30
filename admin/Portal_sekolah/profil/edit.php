5926d240f8a9da220246c86ac700cab0c3e80936
asep2205 <asepsukanda22@gmail.com>
2022-12-30 11:10:24 +0700

    add dashboard/admin

---
+++ admin/Portal_sekolah/profil/edit.php
@@ -0,0 +1,113 @@
+<div class="container-fluid">
+				    <div class="row page-titles mx-0">
+                    <div class="col-sm-6 p-md-0">
+                        <div class="welcome-text">
+                            <h4>Profil  Sekolah</h4>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 p-md-0 justify-content-sm-end mt-2 mt-sm-0 d-flex">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="index.html">Portal Sekolah</a></li>
+                            <li class="breadcrumb-item active"><a href="javascript:void(0);">Profil Sekolah</a></li>
+                            <li class="breadcrumb-item active"><a href="javascript:void(0);">Edit Data</a></li>
+                        </ol>
+                    </div>
+                </div>
+                <div class="row">
+					<div class="col-xl-3 col-xxl-6 col-lg-6 col-md-6">
+						<div class="card">
+							
+							<div class="card-body">
+								<h4><a href="about-courses.html">Edit Profil Sekolah</a></h4>
+								<ul class="list-group mb-3 list-group-flush">
+									<li class="list-group-item px-0 d-flex justify-content-between"><span class="mb-0 text-muted">NPSN </span>
+										<a href="javascript:void(0);"><strong>230</strong></a></li>
+									<li class="list-group-item px-0 d-flex justify-content-between"><span class="mb-0 text-muted">NSS </span>
+										<a href="javascript:void(0);"><strong>230</strong></a></li>
+									<li class="list-group-item px-0 d-flex justify-content-between"><span class="mb-0 text-muted">Nama </span>
+										<a href="javascript:void(0);"><strong>230</strong></a></li>
+									<li class="list-group-item px-0 d-flex justify-content-between"><span class="mb-0 text-muted">Akreditasi</span>
+										<a href="javascript:void(0);"><strong>230</strong></a></li>
+									<li class="list-group-item px-0 d-flex justify-content-between"><span class="mb-0 text-muted">Alamat </span>
+										<a href="javascript:void(0);"><strong>230</strong></a></li>
+									<li class="list-group-item px-0 d-flex justify-content-between"><span class="mb-0 text-muted">Kodepos </span>
+									<a href="javascript:void(0);"><strong>230</strong></a></li>
+									<li class="list-group-item px-0 d-flex justify-content-between"><span class="mb-0 text-muted">Nomer Telpon </span>
+										<a href="javascript:void(0);"><strong>230</strong></a></li>
+									<li class="list-group-item px-0 d-flex justify-content-between"><span class="mb-0 text-muted">Nomer Faks </span>
+									<a href="javascript:void(0);"><strong>230</strong></a></li>
+
+									<li class="list-group-item px-0 d-flex justify-content-between"><span class="mb-0 text-muted">Email </span>
+										<a href="javascript:void(0);"><strong>230</strong></a></li>
+									<li class="list-group-item px-0 d-flex justify-content-between"><span class="mb-0 text-muted">Jenjang </span>
+										<a href="javascript:void(0);"><strong>230</strong></a></li>
+									<li class="list-group-item px-0 d-flex justify-content-between"><span class="mb-0 text-muted">Status </span>
+										<a href="javascript:void(0);"><strong>230</strong></a></li>
+									<li class="list-group-item px-0 d-flex justify-content-between"><span class="mb-0 text-muted">Situs</span>
+										<a href="javascript:void(0);"><strong>230</strong></a></li>
+									<li class="list-group-item px-0 d-flex justify-content-between"><span class="mb-0 text-muted">Lintang </span>
+										<a href="javascript:void(0);"><strong>230</strong></a></li>
+									<li class="list-group-item px-0 d-flex justify-content-between"><span class="mb-0 text-muted">Bujur </span>
+									<a href="javascript:void(0);"><strong>230</strong></a></li>
+									<li class="list-group-item px-0 d-flex justify-content-between"><span class="mb-0 text-muted">Ketinggian </span>
+										<a href="javascript:void(0);"><strong>230</strong></a></li>
+									<li class="list-group-item px-0 d-flex justify-content-between"><span class="mb-0 text-muted">Waktu Belajar </span>
+									<a href="javascript:void(0);"><strong>230</strong></a></li>
+									
+								</ul>
+								
+							</div>
+						</div>
+					</div>
+					<div class="col-xl-3 col-xxl-6 col-lg-6 col-md-6">
+						<div class="card">
+							
+							<div class="card-body">
+								<h4><a href="about-courses.html">Lokasi Sekolah</a></h4>
+								<ul class="list-group mb-3 list-group-flush">
+									<li class="list-group-item px-0 d-flex justify-content-between"><span class="mb-0 text-muted">Kota </span>
+										<a href="javascript:void(0);"><strong>230</strong></a></li>
+									<li class="list-group-item px-0 d-flex justify-content-between"><span class="mb-0 text-muted">Propinsi </span>
+										<a href="javascript:void(0);"><strong>230</strong></a></li>
+									<li class="list-group-item px-0 d-flex justify-content-between"><span class="mb-0 text-muted">Kecamatan </span>
+										<a href="javascript:void(0);"><strong>230</strong></a></li>
+									<li class="list-group-item px-0 d-flex justify-content-between"><span class="mb-0 text-muted">Kelurahan</span>
+										<a href="javascript:void(0);"><strong>230</strong></a></li>
+									<li class="list-group-item px-0 d-flex justify-content-between"><span class="mb-0 text-muted">Kodepos </span>
+										<a href="javascript:void(0);"><strong>230</strong></a></li>
+									
+								</ul>
+								<br>
+								<h4><a href="about-courses.html">Visi & Misi</a></h4>
+								<ul class="list-group mb-3 list-group-flush">
+									<li class="list-group-item px-0 d-flex justify-content-between"><span class="mb-0 text-muted">Visi </span>
+										<a href="javascript:void(0);"><strong>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</strong></a></li>
+									<li class="list-group-item px-0 d-flex justify-content-between"><span class="mb-0 text-muted">Misi </span>
+										<a href="javascript:void(0);"><strong>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</strong></a></li>
+									
+									
+									
+								</ul>
+								
+							</div>
+                            
+						</div>
+					
+					</div>
+					
+					<div class="col-xl-12 col-xxl-12 col-lg-12 col-md-12">
+						<div class="card">
+							
+							<div class="card-body text-center">
+								<button type="submit" class="btn btn-primary">Simpan</button>
+								<a href="#" class="btn btn-warning">Kembali</a>
+								
+							</div>
+                            
+						</div>
+					
+					</div>
+					
+                </div>
+				
+            </div>
